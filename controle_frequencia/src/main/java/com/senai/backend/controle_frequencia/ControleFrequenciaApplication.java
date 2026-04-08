cd687877b6ab45a57289628b77f8c11d1f1849ab
yagojuliao7 <yago.juliao@aluno.senai.br>
2026-04-08 16:32:34 -0300

    enviando api

---
+++ controle_frequencia/src/main/java/com/senai/backend/controle_frequencia/ControleFrequenciaApplication.java
@@ -0,0 +1,14 @@
+package com.senai.backend.controle_frequencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleFrequenciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleFrequenciaApplication.class, args);
+		System.out.println("Saracuruna é bão");
+	}
+
+}
---
+++ controle_frequencia/src/test/java/com/senai/backend/controle_frequencia/ControleFrequenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senai.backend.controle_frequencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleFrequenciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
